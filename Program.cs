a5a633b30ba81fca0bd4252cc31d437b2a927758
cookie17png <62324007+cookie17png@users.noreply.github.com>
2021-05-21 20:14:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace jenk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] mas = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 11, 12 } };
+
+            int rows = mas.GetUpperBound(0) + 1;
+            int columns = mas.Length / rows;
+            // или так
+            // int columns = mas.GetUpperBound(1) + 1;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{mas[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
